37ec96f4c7b729cea278a464751b097cbda19845
Frcc <573316072@qq.com>
2019-09-17 15:58:54 +0800

    fix ios issues

--- ios/Classes/FrappupdatePlugin.m
+++ ios/Classes/FrappupdatePlugin.m
@@ -1,8 +1,18 @@
 #import "FrappupdatePlugin.h"
-#import <frappupdate/frappupdate-Swift.h>
 
 @implementation FrappupdatePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFrappupdatePlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                     methodChannelWithName:@"frappupdate"
+                                     binaryMessenger:[registrar messenger]];
+    FrappupdatePlugin* instance = [[FrappupdatePlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    }
+}
+
 @end
